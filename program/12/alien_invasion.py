c0fa177f4a863c28c754322051436320ccfca21f
Jackie <39873592+Jackie-wx@users.noreply.github.com>
2018-10-08 18:09:20 +0800

    Add files via upload

--- program/12/alien_invasion.py
+++ program/12/alien_invasion.py
@@ -7,12 +7,19 @@ def run_game():
 	screen = pygame.display.set_mode((900,600))
 	pygame.display.set_caption("Alien Invasion")
 	
+	# 设置背景色
+	bg_color = (230,230,230)
+	
 	# 开始游戏的主循环
 	while True:
 		#监视键盘和鼠标事件
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+		
+		# 每次循环时都重新绘制屏幕
+		screen.fill(bg_color)
+		
 		# 让最近绘制的屏幕可见
 		pygame.display.flip()
 
